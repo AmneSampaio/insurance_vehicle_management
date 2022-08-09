5c4a5dbeb76f4714a07ebd00635185761e943e32
AmneSampaio <39910460+AmneSampaio@users.noreply.github.com>
2022-08-09 10:28:20 -0300

    Initial commit

---
+++ vehicles/src/main/java/com/enterprise/manage/vehicles/Application.java
@@ -0,0 +1,14 @@
+package com.enterprise.manage.vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("hey there!");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
